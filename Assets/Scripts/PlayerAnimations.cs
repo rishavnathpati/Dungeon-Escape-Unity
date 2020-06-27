b9c2aa299723c24d7d9ba3402e6c4ed902923083
Rishav Nath Pati <patirishavnath@gmail.com>
2020-06-27 23:42:37 +0530

    Sword Arc added

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -3,12 +3,15 @@
 public class PlayerAnimations : MonoBehaviour
 {
     private Animator anim;
+    private Animator swordAnim;
 
+    public GameObject swordArc;
 
     // Start is called before the first frame update
     private void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        swordAnim = swordArc.GetComponent<Animator>();
     }
 
     public void Move(float move)
@@ -24,5 +27,6 @@ public class PlayerAnimations : MonoBehaviour
     public void Attack()
     {
         anim.SetTrigger("Attack");
+        swordAnim.SetTrigger("Sword Arc");
     }
 }
